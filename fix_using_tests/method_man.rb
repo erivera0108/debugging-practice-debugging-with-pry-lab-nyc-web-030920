ab17204ae303b809d33f8953cf429958556d6da8
Edgar Rivera <edgar.a.rivera08@gmail.com>
2020-02-22 16:46:10 +0000

    Automatically backed up by Learn

--- fix_using_tests/method_man.rb
+++ fix_using_tests/method_man.rb
@@ -5,10 +5,10 @@ def start_game(player1, player2)
   "Hello #{player1} & #{player2}"
 end
 
-def play_game(player2)
+def play_game(player1, player2)
   "#{player1} is better than #{player2}"
   binding.pry
 end
 
 start_game("Rocky", "Bullwinkle")
-play_game("Jay-Z")
+play_game("Jay-Z", "Beyonce")
