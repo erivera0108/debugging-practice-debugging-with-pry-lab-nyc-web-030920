dfceac645663ba6c520e22efcd37c76cb40f6799
Edgar Rivera <edgar.a.rivera08@gmail.com>
2020-02-22 16:36:56 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -15,9 +15,9 @@ def selection(num)
   if num == 1  
     return "YUM YUM MUNCH MUNCH MUNCH"
   elsif num == 2 
-     return "HAM HAM HAM IN MY TUMMY"
+    return "HAM HAM HAM IN MY TUMMY"
   end
-  binding.pry
+  
 end
 
 def runner
